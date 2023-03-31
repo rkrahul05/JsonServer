2ecbdf11951ade2a5ae64604b70fa2055405410e
rahulkumar27 <rahul.kumar27@nagarro.com>
2023-03-31 18:57:12 +0530

    adding env line

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const jsonServer=require("json-server");
 const server=jsonServer.create();
 const router=jsonServer.router("db.json");
 const middlerwares=jsonServer.defaults();
-const port= 8080;
+const port= process.env.PORT || 8080;
 
 server.use(middlerwares);
 server.use(router);
